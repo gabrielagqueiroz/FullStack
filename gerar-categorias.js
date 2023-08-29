97afc026facbd651632c779529e2c6e00fe3979b
GABRIELA PEREIRA GOMES QUEIROZ <GABRIELA6132@DIGITALCOLLEGE.LOCAL>
2023-08-29 19:27:21 -0300

    adc aula8

--- gerar-categorias.js
+++ gerar-categorias.js
@@ -1,6 +1,10 @@
 const database = require('./conexao');
 const {faker} = require('@faker-js/faker/locale/pt_BR');
 
+let nomes = faker.helpers.uniqueArray(
+    
+)
+
 for (let i = 1; i <= 100; i++){
     database.executar(`
         INSERT INTO tb_categoria (nome) VALUES ('${faker.commerce.department()}');
