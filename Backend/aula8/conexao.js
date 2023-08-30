3a993e1b86c1189bceb4eb0cca98cc037d06bc11
GABRIELA PEREIRA GOMES QUEIROZ <GABRIELA6132@DIGITALCOLLEGE.LOCAL>
2023-08-29 21:44:09 -0300

    aula8

--- Backend/aula8/conexao.js
+++ Backend/aula8/conexao.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 const mysql2 = require('mysql2/promise');
 
 async function executar(sql) {
     let conexao = await mysql2.createConnection({
         host: 'localhost',
-        user:'root',
+        user:process.env.DB_USER,
         password: '1234',
         database: 'db_loja',
         port: '3306' //SHOW GLOBAL VARIABLES LIKE 'PORT';
